44930fbcd05498b322e3930856def509fac86730
EviePalaiochorinou <evangeliapal@gmail.com>
2022-01-12 12:05:03 +0000

    Implement the notesView class, fix testing

--- notesView.test.js
+++ notesView.test.js
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 const fs = require('fs');
 const NotesModel = require('./notesModel');
 const NotesView = require('./notesView');
@@ -6,11 +10,12 @@ describe('Notes view', () => {
   it('displays 2 notes', () => {
     document.body.innerHTML = fs.readFileSync('./index.html');
 
-    const notesView = new NotesView();
     const notes = new NotesModel();
-    notes.addNote('Buy milk', 'Go to the gym');
+    notes.addNote('Buy milk');
+    notes.addNote('Go to the gym');
+    const notesView = new NotesView(notes);
     notesView.displayNotes();
 
-    expect(document.querySelectorAll('div.note').length).toBe(2);
+    expect(document.querySelectorAll('div.note').length).toEqual(2);
   });
 });
